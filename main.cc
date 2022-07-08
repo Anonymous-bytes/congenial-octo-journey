98d52cfbcf119df29eeb0ffa4f8a4f4ab5c1b69e
[ Administrator ] <Admin@localhost.localdomain>
2022-07-08 06:33:11 +0000

    [ v1.1.1 ]@ (Fri Jul  8 06:33:11 AM UTC 2022)

--- main.cc
+++ main.cc
@@ -3,6 +3,15 @@
 # include <thread>
 
 
+struct node {
+	public:
+		node(int d,struct node* n) {
+		int data = d;
+		struct node *next = *n;
+		}
+};
+
+
 int main() {
 	std::cout<<"ok"<<std::endl;
 	return 0;
